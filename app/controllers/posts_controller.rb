32e16ade754476599f938153fe3b3519aea7ee29
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-11-14 11:21:51 -0400

    Handle post form submission with errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,21 @@ class PostsController < ApplicationController
     render("new")
   end
 
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to("/posts/#{@post.id}")
+    else
+      render("new")
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :publish_date, :author, :category)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+
+  validates(:title, { presence: true })
+  validates(:content, { presence: true })
+  validates(:publish_date, { presence: true })
+  validates(:author, { presence: true })
+
 end
